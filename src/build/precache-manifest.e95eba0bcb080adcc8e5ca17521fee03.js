4fb1a95c5f1ca887eb3c404c8ac1e259c61171d4
ud20496 <uditbhola018@gmail.com>
2020-04-14 22:38:18 +0530

    first commit

---
+++ src/build/precache-manifest.e95eba0bcb080adcc8e5ca17521fee03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a22c83bb346cc2eacc67d7be987a4b71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c5cd88a69d4659f3390",
+    "url": "/static/css/main.fb5c91dd.chunk.css"
+  },
+  {
+    "revision": "dc5b46c2966c0773b7cc",
+    "url": "/static/js/2.65fa1f5b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.65fa1f5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c5cd88a69d4659f3390",
+    "url": "/static/js/main.17fba6a3.chunk.js"
+  },
+  {
+    "revision": "209d7516a220ea251298",
+    "url": "/static/js/runtime-main.cc5c5978.js"
+  }
+]);
